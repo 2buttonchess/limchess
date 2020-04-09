0fe12709ba7eff7dccec25b2a36e74bceb83f6e9
zlintz14 <zakl14@live.unc.edu>
2020-04-09 01:29:29 -0400

    Finished highlighting moves. Click on piece to highlight it. Possible moves will appear when clicked on. Move mouse around to highlight other squares.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 var game = new Chess()
 var whiteSquareGrey = '#a9a9a9'
 var blackSquareGrey = '#696969'
+var highlightedSquare = null
+var clickedSquareName = ""
 
 function removeGreySquares () {
   $('#board .square-55d63').css('background', '')
@@ -26,6 +28,7 @@ function onClickSquare (square, piece) {
   })
 
   if(moves.length == 0) {
+    $(highlightedSquare).removeClass('highlight2-9c5d2')
     alert("No valid moves")
     return
   } 
@@ -39,15 +42,42 @@ function onClickSquare (square, piece) {
   }
 }
 
+function onMouseoverSquare(square, piece) {
+  // get list of possible moves for this square
+  var moves = game.moves({
+    square: square,
+    verbose: true
+  })
+
+  $('.square-' + square).addClass('highlight2-9c5d2')
+
+}
+
+function onMouseoutSquare(square, piece) {
+  if(square != clickedSquareName)
+    $('.square-' + square).removeClass('highlight2-9c5d2')
+}
+
 $(document).ready(() => {
-  const board = Chessboard('board', 'start');
-  console.log(game.moves({verbose: true}));
+  var config = {
+    position: 'start',
+    onMouseoutSquare: onMouseoutSquare,
+    onMouseoverSquare: onMouseoverSquare
+  }
+  const board = Chessboard('board', config)
 
   $('*[class^="piece"]').click(function() {
+    if(highlightedSquare != null) {
+      $(highlightedSquare).removeClass('highlight2-9c5d2')
+    }
     var piece = event.currentTarget;
     var pieceName = piece.getAttribute('data-piece')
    // square is always third class name and square number (a8, b5, c4, etc) is the last 2 chars of class name
-    var clickedSquare = $(this).parent().get(0).classList[2].substring(7, 9);
+    var squareDiv = $(this).parent().get(0)
+    var clickedSquare = squareDiv.classList[2].substring(7, 9)
+    $(squareDiv).addClass('highlight2-9c5d2')
+    highlightedSquare = squareDiv
+    clickedSquareName = clickedSquare
     onClickSquare(clickedSquare, pieceName)
   })
   
