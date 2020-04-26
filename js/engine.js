3ae447d90bfa88df204bc7569882805b7535de7c
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-04-26 12:03:30 -0400

    add stockfish files from more-stockfish-js-experiments

---
+++ js/engine.js
@@ -0,0 +1,35 @@
+"use strict";
+
+class Engine {
+
+  constructor(onBestMove) {
+    const wasmSupported = typeof WebAssembly === 'object' && WebAssembly.validate(Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00));
+    const stockfish = new Worker(wasmSupported ? 'js/stockfish.wasm.js' : 'js/stockfish.js');
+    // this.onBestMove = onBestMove
+    this.sf = stockfish
+    this.sf.addEventListener('message', e => {
+      console.log(e.data);
+      const words = e.data.split(' ')
+      if (words[0] === 'bestmove')
+        onBestMove(words[1])
+    });
+    this.tell('uci'); // uci initialization
+  }
+
+  position(fen) {
+    this.tell(`position fen ${fen}`)
+  }
+
+  search(maxtimeMS, depth) {
+    this.tell(`go movetime ${maxtimeMS} depth ${depth}`)
+  }
+
+  setSkillLevel(skill) {
+    this.tell(`setoption name Skill Level value ${skill}`)
+  }
+
+  tell(msg) {
+    this.sf.postMessage(msg)
+  }
+
+}
