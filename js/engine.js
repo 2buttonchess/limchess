c6a99e80055bc966e70053ee1691ad56b81d779a
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-04-26 12:00:15 -0400

    handle best moves

--- js/engine.js
+++ js/engine.js
@@ -2,20 +2,34 @@
 
 class Engine {
 
-  constructor() {
+  constructor(onBestMove) {
     const wasmSupported = typeof WebAssembly === 'object' && WebAssembly.validate(Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00));
     const stockfish = new Worker(wasmSupported ? 'js/stockfish.wasm.js' : 'js/stockfish.js');
+    // this.onBestMove = onBestMove
     this.sf = stockfish
-    this.sf.addEventListener('message', this.engine_callback);
+    this.sf.addEventListener('message', e => {
+      console.log(e.data);
+      const words = e.data.split(' ')
+      if (words[0] === 'bestmove')
+        onBestMove(words[1])
+    });
     this.tell('uci'); // uci initialization
+  }
+
+  position(fen) {
+    this.tell(`position fen ${fen}`)
+  }
+
+  search(maxtimeMS, depth) {
+    this.tell(`go movetime ${maxtimeMS} depth ${depth}`)
+  }
+
+  setSkillLevel(skill) {
+    this.tell(`setoption name Skill Level value ${skill}`)
   }
 
   tell(msg) {
     this.sf.postMessage(msg)
-  }
-
-  engine_callback(e) {
-    console.log(e.data);
   }
 
 }
