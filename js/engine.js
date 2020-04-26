df419e117939c4df66f974f225f54439ce95ee8c
D. Ben Knoble <ben.knoble+github@gmail.com>
2020-04-26 11:48:16 -0400

    extract method, move stockfish to js/

--- js/engine.js
+++ js/engine.js
@@ -4,10 +4,14 @@ class Engine {
 
   constructor() {
     const wasmSupported = typeof WebAssembly === 'object' && WebAssembly.validate(Uint8Array.of(0x0, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00));
-    const stockfish = new Worker(wasmSupported ? 'stockfish.wasm.js' : 'stockfish.js');
+    const stockfish = new Worker(wasmSupported ? 'js/stockfish.wasm.js' : 'js/stockfish.js');
     this.sf = stockfish
     this.sf.addEventListener('message', this.engine_callback);
-    this.sf.postMessage('uci'); // uci initialization
+    this.tell('uci'); // uci initialization
+  }
+
+  tell(msg) {
+    this.sf.postMessage(msg)
   }
 
   engine_callback(e) {
